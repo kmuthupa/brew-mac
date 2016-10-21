f6b70c3f45fda0fe77a30d045c0799da284485f9
Karthik Muthupalaniappan <c.karthik.muthupalaniappan@uptake.com>
2016-10-21 15:40:25 -0500

    Add more packages

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,15 +5,27 @@
 # Copyright (c) 2016 Karthik Muthupalaniappan, All Rights Reserved.
 
 
+# productivity
 homebrew_package 'ack'
-homebrew_package 'akka'
 homebrew_package 'archey'
 homebrew_package 'autoconf'
 homebrew_package 'autojump'
 homebrew_package 'the_silver_searcher'
 homebrew_package 'tig'
 homebrew_package 'tmux'
+homebrew_package 'jsonpp'
+
+# utilities
 homebrew_package 'tree'
 homebrew_package 'htop'
-homebrew_package 'jsonpp'
 homebrew_package 'git-extras'
+
+# development
+homebrew_package 'vim'
+homebrew_package 'rbenv'
+homebrew_package 'ruby-build'
+homebrew_package 'mysql'
+
+# cask packages
+homebrew_package 'cask'
+homebrew_package 'cask'
